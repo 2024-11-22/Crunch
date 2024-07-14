66fd0c4d71f20dee7379d7b4965e02e5a0455236
jingtian <lxwk2art@live.com>
2024-07-14 11:39:18 -0500

    30 Add the Base Ability class and The Combo Ability

--- Source/Crunch/Private/GAS/CAbilitySystemComponent.h
+++ Source/Crunch/Private/GAS/CAbilitySystemComponent.h
@@ -15,8 +15,15 @@ class UCAbilitySystemComponent : public UAbilitySystemComponent
 	GENERATED_BODY()
 public:
 	void ApplyInitialEffects();
+	void GiveInitialAbilities();
 
 private:
 	UPROPERTY(EditDefaultsOnly, Category = "Gameplay Effects")
 	TArray<TSubclassOf<UGameplayEffect>> InitialEffects;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay Abilitys")
+	TArray<TSubclassOf<UGameplayAbility>> Abilities;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay Abilitys")
+	TArray<TSubclassOf<UGameplayAbility>> BasicAbilities;
 };
