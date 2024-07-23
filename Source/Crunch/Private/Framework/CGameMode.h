6c54497613a16ea5ef06db7d24b8eb478d41a8e8
jingtian <lxwk2art@live.com>
2024-07-23 12:00:02 -0500

    56 Respawn Player Character At Their Start Spot

--- Source/Crunch/Private/Framework/CGameMode.h
+++ Source/Crunch/Private/Framework/CGameMode.h
@@ -20,8 +20,8 @@ public:
 private:
 	FGenericTeamId GetTeamIDForPlayer(const APlayerController* PlayerController) const;
 
-	AActor* FIndNextStartSpotForTeam(const FGenericTeamId& TeamID) const;
+	AActor* FindNextStartSpotForTeam(const FGenericTeamId& TeamID) const;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Team")
-	TMap<FGenericTeamId, FName> TeamStartSoptTagMap;
+	TMap<FGenericTeamId, FName> TeamStartSpotTagMap;
 };
