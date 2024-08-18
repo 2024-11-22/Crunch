987509a89a693af6f962f7374885986a86b79527
jingtian <lxwk2art@live.com>
2024-08-18 13:49:16 -0500

    117 Apply the Rewards

--- Source/Crunch/Private/GAS/GAP_Dead.h
+++ Source/Crunch/Private/GAS/GAP_Dead.h
@@ -36,4 +36,8 @@ private:
 	float KillerRewardPortion = 0.5f;
 
 	TArray<AActor*> GetRewardTargets() const;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Reward")
+	TSubclassOf<UGameplayEffect> RewardEffect;
+	
 };
