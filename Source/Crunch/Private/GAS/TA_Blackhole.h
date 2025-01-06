551c66fe8f575c2803f0640162a4900edf5f720c
jingtian <lxwk2art@live.com>
2025-01-05 19:27:54 -0600

    216 Implement the Pulling of the Blackhole Target Actor

--- Source/Crunch/Private/GAS/TA_Blackhole.h
+++ Source/Crunch/Private/GAS/TA_Blackhole.h
@@ -23,15 +23,24 @@ public:
 	/** Retrieve team identifier in form of FGenericTeamId */
 	virtual FGenericTeamId GetGenericTeamId() const { return TeamId; }
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void StartTargeting(class UGameplayAbility* Ability) override;
+	virtual void Tick(float DeltaTime) override;
 private:
 	UPROPERTY(Replicated)
 	FGenericTeamId TeamId;
 
 	float PullSpeed;
 	float BlackholeDuration;
+	FTimerHandle BlackholeDurationTimerHandle;
 
 	UPROPERTY(ReplicatedUsing = OnRep_BlackholeRange)
 	float BlackholeRange;
+
+	UPROPERTY(EditDefaultsOnly, Category = "VFX")
+	FName BlackholeVFXOriginVariableName = "Origin";
+
+	UPROPERTY(EditDefaultsOnly, Category = "VFX")
+	class UNiagaraSystem* BlackholeLinkVFX;
 
 	UFUNCTION()
 	void OnRep_BlackholeRange();
@@ -45,10 +54,16 @@ private:
 	UPROPERTY(VisibleDefaultsOnly, Category = "Component")
 	class UParticleSystemComponent* VFXComponent;
 
-
 	UFUNCTION()
 	void ActorInBlackholeRange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 
 	UFUNCTION()
 	void ActorLeftBlackholeRange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	void TryAddTarget(AActor* OtherTarget);
+	void RemoveTarget(AActor* OtherTarget);
+
+	TMap<AActor*, class UNiagaraComponent*> ActorsInRangeMap;
+
+	void StopBlackhole();
 };
