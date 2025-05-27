53670b1b88e69c35462badc24fb35e73f8d46ced
jingtian <lxwk2art@live.com>
2025-05-27 17:34:02 -0500

    254 Configure EOS in Our Project

--- Source/Crunch/Crunch.Build.cs
+++ Source/Crunch/Crunch.Build.cs
@@ -10,7 +10,7 @@ public class Crunch : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags", "UMG", "Slate", "SlateCore", "AIModule", "Niagara"});
+		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags", "UMG", "Slate", "SlateCore", "AIModule", "Niagara", "OnlineSubsystem","OnlineSubsystemEOS","OnlineSubsystemUtils","Networking","HTTP","Json"} );
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
