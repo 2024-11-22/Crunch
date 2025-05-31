56281f1db2a3815fad96126eafbd3285560788d9
jingtian <lxwk2art@live.com>
2025-05-31 16:02:43 -0500

    270 Using the HTTP server to Launch and Instance of the Server  Upon Request

--- Coordinator/coordinator.py
+++ Coordinator/coordinator.py
@@ -9,11 +9,32 @@ app=Flask(__name__)
 # TODO: Remove when using docker in the future
 nextAvailablePort = 7777
 
+def CreateSeverLocalTest(sessionName, sessionSearchId):
+    global nextAvailablePort
+    subprocess.Popen([
+        "F:/UnrealSrc/UnrealEngine/Engine/Binaries/Win64/UnrealEditor.exe",
+        "D:/CompleteGameDevSeries06_Crunch/Crunch/Crunch.uproject" ,
+        "-server",
+        "-log",
+        '-epicapp="ServerClient"',
+        f'-SESSION_NAME="{sessionName}"',
+        f'-SESSION_SEARCH_ID="{sessionSearchId}"',
+        f'-PORT={nextAvailablePort}'
+    ])
+
+    usedPort = nextAvailablePort
+    nextAvailablePort += 1
+    return usedPort
+
+
 @app.route('/Sessions', methods=['POST'])
 def CreateSever():
     print(dict(request.headers))
 
-    port = nextAvailablePort
+    sessionName = request.get_json().get(SESSION_NAME_KEY)
+    sessionSearchId = request.get_json().get(SESSION_SEARCH_ID_KEY)
+
+    port = CreateSeverLocalTest(sessionName, sessionSearchId)
     return jsonify({"status": "success", PORT_KEY: port}), 200
 
 if __name__ == "__main__":
